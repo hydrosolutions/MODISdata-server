a8aea260fef81ddcd65b6901a50ad89a192a2847
julesair <jules.henze@gmail.com>
2018-03-06 12:09:33 +0100

    ...

--- config.R
+++ config.R
@@ -1,8 +1,8 @@
 #file:downloadconf.R: settings for download.R,
 MODIS_DATASTORAGE = "/home/jules/MODISStorage/"
-DATASTORAGE_LOC = "/home/jules/liveSNOWdata/" 
+DATASTORAGE_LOC = "/home/jules/liveNDVIdata/" 
 DATABASE_LOC = "/home/jules/Desktop/MODIS_Service/database.csv"
 maxDOWNLOADchunk = 30
 TIMESERIES_DEFAULT_NAME = "timeseries.csv"
 GEOTIFF_COMPRESSION = FALSE
-GEOTIFF_PROCESSOR = "modis_10A1.R"
+GEOTIFF_PROCESSOR = "modis_13Q1.R"
