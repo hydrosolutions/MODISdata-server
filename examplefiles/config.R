391a28d1a9402313cd21c6eacc02fe625eb649dd
julesair <jules.henze@gmail.com>
2018-03-12 07:57:44 +0100

    added examplefiles

---
+++ examplefiles/config.R
@@ -0,0 +1,8 @@
+#file:downloadconf.R: settings for download.R,
+MODIS_DATASTORAGE = "/home/jules/MODISStorage/"
+DATASTORAGE_LOC = "/home/jules/liveSNOWdata/" 
+DATABASE_LOC = "/home/jules/Desktop/Hydromet/MODISsnow_server/database.csv"
+maxDOWNLOADchunk = 3
+TIMESERIES_DEFAULT_NAME = "timeseries.csv"
+GEOTIFF_COMPRESSION = TRUE
+GEOTIFF_PROCESSOR = "modis_10A1.R"
